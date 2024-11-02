8438f4ccc9d4bf0af3f4fa91f0be56889c9d339f
Peter Nearing <me@peternearing.ca>
2024-11-02 11:16:07 -0400

    Ensure correct ownership of copied data in start script
    Added chown commands to set ownership of directories /nagios_etc, /nagios_var, /nagiosgraph_etc, and /nagiosgraph_var after copying example data. This guarantees that the Nagios services will have the correct permissions to read and write to these directories.

--- start.sh
+++ start.sh
@@ -4,21 +4,25 @@
 if [ -z "$(ls -A /nagios_etc)" ]; then
     echo "Started with empty ETC, copying example data in-place"
     cp -Rp /orig/etc/* /nagios_etc/
+    chown ${NAGIOS_USER}:${NAGIOS_GROUP} /nagios_etc
 fi
 
 if [ -z "$(ls -A /nagios_var)" ]; then
     echo "Started with empty VAR, copying example data in-place"
     cp -Rp /orig/var/* /nagios_var/
+    chown ${NAGIOS_USER}:${NAGIOS_GROUP} /nagios_var
 fi
 
 if [ -z "$(ls -A /nagiosgraph_etc)" ]; then
     echo "Started with empty /nagiosgraph_etc, copying example data in-place"
     cp -Rp /orig/graph-etc/* /nagiosgraph_etc/
+    chown ${NAGIOS_USER}:${NAGIOS_GROUP} /nagiosgraph_etc
 fi
 
 if [ -z "$(ls -A /nagiosgraph_var)" ]; then
     echo "Started with empty /nagiosgraph_var, copying example data in-place"
     cp -Rp /orig/graph-var/* /nagiosgraph_var/
+    chown ${NAGIOS_USER}:${NAGIOS_GROUP} /nagiosgraph_var
 fi
 
 # Update configuration files with the variable values:
